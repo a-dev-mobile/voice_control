ac65c72d927f419a66a60c6aa290ad2fc3bffae2
Dmitriy Trofimov <dmitry.trofimov@aventus.work>
2022-05-27 21:00:59 +0300

    feat: opens the mail - ok

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
-
+import os
+import subprocess
+from turtle import listen
 import speech_recognition
 
 sr = speech_recognition.Recognizer()
+sr.pause_threshold = 0.5
+
+command_dict = {'commands': {
+    'greeting': ['почта', 'открой почту', 'почту', ]}}
 
 
-with speech_recognition.Microphone() as mic:
-    sr.adjust_for_ambient_noise(source=mic, duration=0.5)
-    audio = sr.listen(source=mic)
+def listen_command():
 
-    query = sr.recognize_google(audio_data=audio, language='ru-RU').lower()
-print(query)
+    try:
+        with speech_recognition.Microphone() as mic:
+            sr.adjust_for_ambient_noise(source=mic, duration=0.5)
+            audio = sr.listen(source=mic)
+
+            query = sr.recognize_google(
+                audio_data=audio, language='ru-RU').lower()
+            print("Said >>> "+ query)
+            return query
+    except speech_recognition.UnknownValueError:
+        return '!!! Voice command error !!!'
+
+
+def greeting():
+    current_dir = "c:/trofimov/APP/ThunderbirdPortable/"
+    p = subprocess.Popen(os.path.join(
+        current_dir, "ThunderbirdPortable.exe"), cwd=current_dir)
+    return 'Welcome to the Google  '
+
+
+def main():
+    query = listen_command()
+
+    for k, v in command_dict['commands'].items():
+        if query in v:
+            print(globals()[k]())
+
+
+if __name__ == '__main__':
+    main()
